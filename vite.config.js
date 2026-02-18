ae0d2a7ae1fc5bac7333aaa826a44276d2feb1eb
Health Portal <yourEmail@example.com>
2026-02-18 20:10:36 +0200

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/apple-health-portal/',
   plugins: [react()],
   server: {
     host: '127.0.0.1',
